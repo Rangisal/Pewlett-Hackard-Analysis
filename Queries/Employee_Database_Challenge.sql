00b07f074cf4485b980ce843594c9909c229c66b
Rangisal <rangika.rowell@gmail.com>
2021-12-18 22:41:44 -0500

    update the challenge

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -37,6 +37,28 @@ ORDER BY COUNT(title) DESC;
 
 SELECT * FROM retiring_titles;
 
+--Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program.
+
+SELECT DISTINCT ON (emp_no)
+       e.emp_no,
+	   e.first_name,
+	   e.last_name,
+	   e.birth_date,
+	   de.from_date,
+	   de.to_date,
+	   ti.title
+INTO mentorship_eligibility
+FROM employees as e
+LEFT JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+LEFT JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibility;
+
 	   
 	   
 	   
